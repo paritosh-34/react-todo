dca4ce087df0c5235a76859deb61b5e2799ccc8e
Paritosh Batish <batishparitosh2@gmail.com>
2020-05-24 09:51:39 +0530

    First Commit

---
+++ precache-manifest.6d6638731b5ca4d14de508704e7eabb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecca8e02607f5b16878fd965bd233c83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "712248598b6593bcb8ef",
+    "url": "/static/css/main.f798771c.chunk.css"
+  },
+  {
+    "revision": "a611e21dca59ff616b54",
+    "url": "/static/js/2.25fe6d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25fe6d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712248598b6593bcb8ef",
+    "url": "/static/js/main.cb58b840.chunk.js"
+  },
+  {
+    "revision": "9b2bc38bd5f3e3e66ef4",
+    "url": "/static/js/runtime-main.4ac958bd.js"
+  }
+]);
